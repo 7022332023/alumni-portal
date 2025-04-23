46fe3bf1b2aebedba019166df422518c473f6790
PREM M BADERA <157642588+7022332023@users.noreply.github.com>
2025-04-23 17:50:07 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignment 2 | Index</title>
+
+    <link rel="stylesheet" href="css/styles.css">
+
+    <!-- Bootstrap CSS --><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Bootstrap Icons --><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1>S2P Portal</h1>
+    </div>
+    
+    <div class="container mb-5 py-4 px-4 mainView bg-white">
+        <div class="row mb-5">
+
+            <!-- Name, ID, Email -->
+            <div class="col">
+                <div class="col mb-1">
+                    <h2>Prem M Badera</h2>
+                </div>
+                <div class="col mb-1">
+                    <span><strong>Student ID:</strong> 1JS22CS120</span>
+                </div>
+                <div class="col">
+                    <span><strong>Email:</strong> <a class="link-underline link-underline-opacity-0" href="mailto:prembadera1@gmail.com">prembadera1@gmail.com</a></span>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+                <div class="col mb-1">
+                    <h2>Nishanth V</h2>
+                </div>
+                <div class="col mb-1">
+                    <span><strong>Student ID:</strong>1JS22CS101</span>
+                </div>
+                <div class="col">
+                    <span><strong>Email:</strong> <a class="link-underline link-underline-opacity-0" href="mailto:nishanthvishwas44@gmail.com">nishanthvishwas44@gmail.com</a></span>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+                <div class="col mb-1">
+                    <h2>Nikitha G Shenoy</h2>
+                </div>
+                <div class="col mb-1">
+                    <span><strong>Student ID:</strong>1JS22CS098</span>
+                </div>
+                <div class="col">
+                    <span><strong>Email:</strong> <a class="link-underline link-underline-opacity-0" href="mailto:nikithashenoy123@gmail.com">nikithashenoy123@gmail.com</a></span>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+                <div class="col mb-1">
+                    <h2>Rakshitha S</h2>
+                </div>
+                <div class="col mb-1">
+                    <span><strong>Student ID:</strong> 1JS22CS126</span>
+                </div>
+                <div class="col">
+                    <span><strong>Email:</strong> <a class="link-underline link-underline-opacity-0" href="mailto:rakshitharak8600@gmail.com">rakshitharak8600@gmail.com</a></span>
+                </div>
+            </div>
+        </div>
+        <hr/>
+        
+
+            <div class="row justify-content-center mt-5">
+                <div class="col-auto">
+                    <a role="button" href="main.php" class="btn btn-primary py-2">Main Page</a>
+                </div>
+                <div class="col-auto">
+                    <a role="button" href="about.php" class="btn btn-outline-primary py-2">About this assignment</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!-- Boostrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
